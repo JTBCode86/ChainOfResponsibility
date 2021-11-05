19041ebc01a1fbc7aba1976e6fb23d3be548b7ea
Jean Barbosa <jeanbarbosa.it@gmail.com>
2021-11-04 22:59:38 -0300

    Adicionar arquivos de projeto.

---
+++ Middlewares/CheckWeakPassword.cs
@@ -0,0 +1,18 @@
+﻿using ChainOfResponsibility.Servers;
+using System;
+
+namespace ChainOfResponsibility.Middlewares
+{
+    class CheckWeakPassword : Middleware
+    {
+        public override bool Check(string email, string password)
+        {
+            if (password.Equals("123456"))
+            {
+                Console.WriteLine("ATENÇÃO: Considere usar uma senha mais segura!");
+            }
+
+            return CheckNext(email, password);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using ChainOfResponsibility.Middlewares;
+using ChainOfResponsibility.Servers;
+using System;
+
+namespace ChainOfResponsibility
+{
+    class Program
+    {
+        private static Server server;
+        static void Init() 
+        {
+            server = new Server();
+            server.RegisterUser("master@hcode.com.br","123123sdkjh@%$");
+            server.RegisterUser("panda@hcode.com.br", "123455");
+            server.RegisterUser("minipanda@hcode.com.br", "123456");
+
+            Middleware middleware = new CheckUserMiddleware(server);
+
+            middleware.LinkWith(new CheckPermissionMiddleware());
+            middleware.LinkWith(new CheckWeakPassword());
+
+            server.SetMiddleware(middleware);
+        }
+
+        static void Main(string[] args)
+        {
+            Init();
+            Boolean done = false;
+
+            do
+            {
+                Console.WriteLine("Digite seu e-mail: ");
+                string email = Console.ReadLine();
+
+                Console.WriteLine("Disite sua senha: ");
+                string password = Console.ReadLine();
+                done=server.LogIng(email, password);
+
+            } while (!done);
+
+            Console.ReadLine();
+        }
+    }
+}
